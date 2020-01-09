6d6d94b6403a9ca26ef0db23dfa0160e0ebaf07e
Pascal <pascalvanleeuwen604@gmail.com>
2020-01-09 01:21:22 +0000

    Added 1_queries folder.

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -21,10 +21,10 @@ CREATE TABLE properties (
   description TEXT,
   thumbnail_photo_url VARCHAR(2083) NOT NULL,
   cover_photo_url VARCHAR(2083) NOT NULL,
-  cost_per_night SMALLINT NOT NULL DEFAULT 0,
-  parking_spaces SMALLINT NOT NULL DEFAULT 0,
-  number_of_bathrooms SMALLINT NOT NULL DEFAULT 0,
-  number_of_bedrooms SMALLINT NOT NULL DEFAULT 0,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  parking_spaces INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
 
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE rates (
 
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-  cost_per_night SMALLINT NOT NULL
+  cost_per_night INTEGER NOT NULL
 );
 
 CREATE TABLE reservations (
@@ -58,7 +58,7 @@ CREATE TABLE property_reviews (
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
 
-  rating SMALLINT NOT NULL DEFAULT 0,
+  rating INTEGER NOT NULL DEFAULT 0,
   message TEXT
 );
 
@@ -68,6 +68,6 @@ CREATE TABLE guest_reviews (
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
-  rating SMALLINT NOT NULL DEFAULT 0,
+  rating INTEGER NOT NULL DEFAULT 0,
   message TEXT
 );
